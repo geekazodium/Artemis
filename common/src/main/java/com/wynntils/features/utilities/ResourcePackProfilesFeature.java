c7d1140706641fd4bbdc4b656b0e7c313a17ca89
Geekazodium <59382423+geekazodium@users.noreply.github.com>
2023-08-24 16:15:38 +0800

    feat: Resource Pack Profiles

---
+++ common/src/main/java/com/wynntils/features/utilities/ResourcePackProfilesFeature.java
@@ -0,0 +1,108 @@
+package com.wynntils.features.utilities;
+
+import com.wynntils.core.consumers.features.Feature;
+import com.wynntils.core.persisted.Persisted;
+import com.wynntils.core.persisted.config.Category;
+import com.wynntils.core.persisted.config.Config;
+import com.wynntils.core.persisted.config.ConfigCategory;
+import com.wynntils.mc.event.GetPackRepositoryEvent;
+import com.wynntils.mc.event.ScreenInitEvent;
+import com.wynntils.mc.event.TitleScreenInitEvent;
+import com.wynntils.utils.mc.McUtils;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.events.GuiEventListener;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.TitleScreen;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.packs.repository.PackRepository;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ConfigCategory(Category.UTILITIES)
+public class ResourcePackProfilesFeature extends Feature {
+
+    @Persisted
+    private final Config<Boolean> usingWynncraftTexturesTitleScreen = new Config<>(true);
+    @Persisted
+    private final Config<Boolean> usingWynncraftTexturesOtherServers = new Config<>(false);
+    @Persisted
+    private final Config<Boolean> usingWynncraftTexturesSinglePlayer = new Config<>(false);
+
+    @Persisted
+    private final Config<List<String>> WynncraftSelectedTextures = new Config<>(new ArrayList<>());
+
+    public PackRepository wynncraftPackRepository;
+
+    private int enableTexturesButtonHashcode = -1;
+    private Button enableWynntilsTexturesButton = null;
+
+    @SubscribeEvent
+    public void onInitTitle(TitleScreenInitEvent event) {
+        TitleScreen titleScreen = event.getTitleScreen();
+        AddWynnTexturesButton(titleScreen);
+    }
+
+    @SubscribeEvent
+    public void onInitScreen(ScreenInitEvent event) {
+        if (event.getScreen() instanceof TitleScreen titleScreen) {
+            AddWynnTexturesButton(titleScreen);
+        }
+    }
+
+    private void toggleTitleScreenPack(){
+        usingWynncraftTexturesTitleScreen.setValue(!usingWynncraftTexturesTitleScreen.get());
+        this.setUsingWynncraftTextures(usingWynncraftTexturesTitleScreen.get());
+    }
+
+    private void AddWynnTexturesButton(TitleScreen titleScreen) {
+        int titleScreenWynncraftButtonY = titleScreen.height / 4 + 48 + 24;
+        int buttonY = titleScreenWynncraftButtonY + 24;
+        int buttonX = titleScreen.width / 2 + 104;
+        if (screenHasEnableTexturesButton(titleScreen)) {
+            enableWynntilsTexturesButton.setPosition(buttonX, buttonY);
+            return;
+        }
+
+        enableWynntilsTexturesButton =
+                titleScreen.addRenderableWidget(Button.builder(Component.literal("textures"), button -> toggleTitleScreenPack())
+                        .bounds(buttonX, buttonY, 20, 20)
+                        .build());
+        enableTexturesButtonHashcode = enableWynntilsTexturesButton.hashCode();
+    }
+
+    private boolean screenHasEnableTexturesButton(Screen screen) {
+        return screen.children.stream().anyMatch(this::isGuiElementButton);
+    }
+
+    private boolean isGuiElementButton(GuiEventListener child) {
+        if (!(child instanceof Button button)) return false;
+        if (button.hashCode() != enableTexturesButtonHashcode) return false;
+        return button == enableWynntilsTexturesButton;
+    }
+
+    @SubscribeEvent
+    public void onGetResourcePack(GetPackRepositoryEvent event){
+        if(usingWynncraftTexturesTitleScreen.get())event.setOverridePackRepo(this.wynncraftPackRepository);
+    }
+
+
+    private boolean usingWynncraftTextures = false;
+    public boolean getUsingWynncraftTextures(){
+        return usingWynncraftTextures;
+    }
+    private void setUsingWynncraftTextures(boolean usingWynncraftTextures) {
+        boolean pendingPackReload = this.usingWynncraftTextures != usingWynncraftTextures;
+        this.usingWynncraftTextures = usingWynncraftTextures;
+        if(pendingPackReload){
+            McUtils.mc().reloadResourcePacks();
+        }
+    }
+
+
+    @Override
+    public void onEnable() {
+        System.out.println("testing 1234");
+    }
+}
--- common/src/main/java/com/wynntils/mc/mixin/MinecraftMixin.java
+++ common/src/main/java/com/wynntils/mc/mixin/MinecraftMixin.java
@@ -8,19 +8,30 @@ import com.llamalad7.mixinextras.sugar.Share;
 import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import com.wynntils.core.events.MixinHelper;
 import com.wynntils.mc.event.DisplayResizeEvent;
+import com.wynntils.mc.event.GetPackRepositoryEvent;
 import com.wynntils.mc.event.ScreenClosedEvent;
 import com.wynntils.mc.event.ScreenOpenedEvent;
 import com.wynntils.mc.event.TickAlwaysEvent;
 import com.wynntils.mc.event.TickEvent;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.main.GameConfig;
+import net.minecraft.server.packs.repository.PackRepository;
+import net.minecraftforge.eventbus.api.Event;
+import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Minecraft.class)
 public abstract class MinecraftMixin {
+    @Shadow @Final private PackRepository resourcePackRepository;
+
     @Inject(method = "setScreen(Lnet/minecraft/client/gui/screens/Screen;)V", at = @At("RETURN"))
     private void setScreenPost(Screen screen, CallbackInfo ci, @Share("oldScreen") LocalRef<Screen> oldScreen) {
         if (screen == null) {
@@ -53,4 +64,13 @@ public abstract class MinecraftMixin {
     private void resizeDisplayPost(CallbackInfo ci) {
         MixinHelper.postAlways(new DisplayResizeEvent());
     }
+
+    @Redirect(method = {"rollbackResourcePacks","clearResourcePacksOnError","getResourcePackRepository","reloadResourcePacks(Z)Ljava/util/concurrent/CompletableFuture;"},at = @At(value = "FIELD",target = "Lnet/minecraft/client/Minecraft;resourcePackRepository:Lnet/minecraft/server/packs/repository/PackRepository;",opcode = Opcodes.GETFIELD))
+    private PackRepository overrideGetPackRepository(Minecraft minecraft){
+        GetPackRepositoryEvent getPackRepositoryEvent = new GetPackRepositoryEvent();
+        MixinHelper.postAlways(getPackRepositoryEvent);
+        PackRepository overrideValue = getPackRepositoryEvent.getOverridePackRepository();
+        return (overrideValue == null)?resourcePackRepository:overrideValue;
+    }
+
 }
