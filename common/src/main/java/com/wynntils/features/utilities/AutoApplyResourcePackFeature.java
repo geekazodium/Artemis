ea234fba088246bb40c5160ea85574e3cc7250ad
Geekazodium <59382423+geekazodium@users.noreply.github.com>
2023-08-24 16:15:38 +0800

    Added Placeholder Button to access wynntils texture settings

--- common/src/main/java/com/wynntils/features/utilities/AutoApplyResourcePackFeature.java
+++ common/src/main/java/com/wynntils/features/utilities/AutoApplyResourcePackFeature.java
@@ -8,7 +8,14 @@ import com.wynntils.core.components.Services;
 import com.wynntils.core.consumers.features.Feature;
 import com.wynntils.core.persisted.config.Category;
 import com.wynntils.core.persisted.config.ConfigCategory;
+import com.wynntils.features.ui.WynncraftButtonFeature;
 import com.wynntils.mc.event.ResourcePackEvent;
+import com.wynntils.mc.event.ScreenInitEvent;
+import com.wynntils.mc.event.TitleScreenInitEvent;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.screens.TitleScreen;
+import net.minecraft.client.multiplayer.ServerData;
+import net.minecraft.network.chat.Component;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 @ConfigCategory(Category.UTILITIES)
@@ -27,5 +34,45 @@ public class AutoApplyResourcePackFeature extends Feature {
             // Use this resource pack as our preloaded pack
             Services.ResourcePack.setRequestedPreloadHash(packHash);
         }
+    }
+
+    private int enableTexturesButtonHashcode = -1;
+    private Button enableWynntilsTexturesButton = null;
+
+    @SubscribeEvent
+    public void onInitTitle(TitleScreenInitEvent event){
+        TitleScreen titleScreen = event.getTitleScreen();
+        AddWynnTexturesButton(titleScreen);
+    }
+
+    @SubscribeEvent
+    public void onInitScreen(ScreenInitEvent event){
+        if(event.getScreen() instanceof TitleScreen titleScreen){
+            AddWynnTexturesButton(titleScreen);
+        }
+    }
+
+    private void AddWynnTexturesButton(TitleScreen titleScreen) {
+        int titleScreenWynncraftButtonY = titleScreen.height / 4 + 48 + 24;
+        int buttonY = titleScreenWynncraftButtonY + 24;
+        int buttonX = titleScreen.width / 2 + 104;
+        if (screenHasEnableTexturesButton(titleScreen)) {
+            //titleScreen.removeWidget(enableWynntilsTexturesButton);
+            enableWynntilsTexturesButton.setPosition(buttonX,buttonY);
+            return;
+        }
+
+        enableWynntilsTexturesButton = titleScreen.addRenderableWidget(Button.builder(Component.literal("textures"), button ->{
+
+        }).bounds(buttonX, buttonY,20,20).build());
+        enableTexturesButtonHashcode = enableWynntilsTexturesButton.hashCode();
+    }
+
+    private boolean screenHasEnableTexturesButton(TitleScreen titleScreen) {
+        return titleScreen.children.stream().anyMatch(child -> {
+            if (!(child instanceof Button button)) return false;
+            if (button.hashCode() != enableTexturesButtonHashcode) return false;
+            return button == enableWynntilsTexturesButton;
+        });
     }
 }
